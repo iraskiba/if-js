0f25ad5aad931fed36493cb46e7381598ba09eb3
iraskiba <bruxoveczkayai@gmail.com>
2023-08-23 23:29:05 +0300

    added problem solving

--- src/js/lesson15.js
+++ src/js/lesson15.js
@@ -1,39 +1,24 @@
-const formEl = document.getElementById('form');
 const inputFile = document.getElementById('input');
-formEl.addEventListener('submit', async (event) => {
-  event.preventDefault();
+const container = document.querySelector('.container');
+const addPfoto = document.getElementById('add');
 
-  const url = ' https://if-student-api.onrender.com/api/file';
-
-  const file = event.target[0];
-
-  const fetchOptions = {
-    method: 'POST',
-    body: new FormData(formEl),
-
-  };
-  const resp = await fetch(url, fetchOptions)
-    .then(response => response.json())
-        .then(data => {
-          console.log(data);
-        })
-        .catch(error => console.log(error.message));
-
-    });
-
-inputFile.addEventListener('change',(event) => {
-  const files = event.target.files[0];
+container.addEventListener('click', (event) => {
+  inputFile.click();
+});
+inputFile.addEventListener('change', (event) =>{
+  const file = event.target.files[0];
   const reader = new FileReader();
   reader.onload = function (event) {
-    const  getUrl = event.target.result;
+    const  imgUrl = event.target.result;
     const imgElement = document.createElement('img');
-    imgElement.src = getUrl;
-    document.body.appendChild(imgElement);
-    console.log(getUrl);
-  };
-  reader.readAsDataURL(files);
+    imgElement.src = imgUrl;
+    imgElement.classList.add('img');
+    container.innerHTML = '';
+    container.appendChild(imgElement);
 
+    console.log(imgUrl);
+};
+reader.readAsDataURL(file);
 });
-
 
 
