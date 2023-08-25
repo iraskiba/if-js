7deb51aebedb2cdbfc750f6193f0a1c1abe3b61d
iraskiba <bruxoveczkayai@gmail.com>
2023-08-25 23:32:32 +0300

    added two solutions

--- src/js/lesson15.js
+++ src/js/lesson15.js
@@ -22,3 +22,28 @@ reader.readAsDataURL(file);
 });
 
 
+// 15 урок первая часть
+
+const formEl = document.getElementById('form');
+const lesson15 = document.getElementById('input');
+formEl.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const url = ' https://if-student-api.onrender.com/api/file';
+
+  const file = event.target[0];
+
+  const fetchOptions = {
+    method: 'POST',
+    body: new FormData(formEl),
+    mode: 'no-cors',
+
+  };
+  const resp = await fetch(url, fetchOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.log(error.message));
+
+});
